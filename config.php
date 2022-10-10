85d4e8090038730ab632e6dfbfa6d299ae9f5b23
jpoliveiradev <info1joaopaulo@example.com>
2022-10-10 11:17:28 -0300

    Login no Heroku

--- config.php
+++ config.php
@@ -1,12 +1,25 @@
 <?php
 
 
-$dbHost = 'Localhost';
-$dbUserame = 'root';
-$dbPassword = '';
-$dbName = 'dblivraria';
+$dbHost = 'us-cdbr-east-06.cleardb.net';
+$dbUserame = 'b4ee5d50e48637';
+$dbPassword = '9ef676b4';
+$dbName = 'heroku_f3a0f7238f7a0b5';
 
-$conexao = new mysqli($dbHost, $dbUserame, $dbPassword, $dbName);
+$conexao = new mysqli(
+    $dbHost,
+    $dbUserame,
+    $dbPassword,
+    $dbName
+);
+
+
+// $dbHost = 'Localhost';
+// $dbUserame = 'root';
+// $dbPassword = '';
+// $dbName = 'dblivraria';
+
+// $conexao = new mysqli($dbHost, $dbUserame, $dbPassword, $dbName);
 
 /* if ($conexao->connect_errno) {
     echo "<h1>Erro!</h1>";
